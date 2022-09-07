5626a96bac786e03f5c3fc785f7e062a25dd58be
AlbaMoran <alba.morann@gmail.com>
2022-09-07 19:37:35 -0300

    feat: Desafio#4 ItemCount.js

--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import ItemCount from './ItemCount'
+import swal from 'sweetalert';
+
 
 function ItemListContainer({greeting}){
+
+    const onAdd = () => {
+
+        swal({
+        title: `Se agregó correctamente al carrito`,
+        icon: 'success',
+        button: "Aceptar",
+        timmer: "2000"
+      
+    })}
+
+
+
+   
 
     return (
             <>
             <h1 style={styles.h1}>{greeting}</h1>
+            <ItemCount stock ="10" initial="0" onAdd={onAdd} />
+
             </>
     )
 }
